bdf7143d096b45b73c8794c4e65179dec6d0e549
korgrunt <naim.aouaichia@gmail.com>
2023-12-04 12:26:54 +0100

    exec command suricata

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 import binascii
 import string
 import sys
+import os
 import json
 from base64 import b64encode
 from src.help import *
@@ -13,34 +14,42 @@ from src.visibility_mode import *
 def parse_arguments(argv):
     arguments_container = {}
     arguments_container['option'] = argv[1]
+    arguments_container['pcap_file'] = argv[2]
+    arguments_container['output_file'] = argv[3] if len(argv) == 4 else False
     return arguments_container
+
+def exec_suricata_on_pcap(arguments_parsed):
+    try:
+        os.system(f"suricata -r {arguments_parsed['pcap_file']}")
+    except:
+        print(f"Can't execute shell commande $> suricata -r {arguments_parsed['pcap_file']}")
+        sys.exit(1)
 
 def exec_suricata_analyzr(arguments_parsed):
     if(arguments_parsed['option'] == '-h' or arguments_parsed['option'] == '--help'):
-        return print_help_page()
+        print_help_page()
     elif(arguments_parsed['option'] == '-a' or arguments_parsed['option'] == '--all'):
-        return analyze_all_mode()
+        exec_suricata_on_pcap(arguments_parsed)
+        analyze_visibility_mode(arguments_parsed)
+        analyze_detection_mode(arguments_parsed)
     elif(arguments_parsed['option'] == '--version'):
         print_version()
     elif(arguments_parsed['option'] == '-v' or arguments_parsed['option'] == '--visibility'):
-        return analyze_visibility_mode(arguments_parsed)
+        exec_suricata_on_pcap(arguments_parsed)
+        analyze_visibility_mode(arguments_parsed)
     elif(arguments_parsed['option'] == '-d' or arguments_parsed['option'] == '--detection'):
-        return analyze_detection_mode(arguments_parsed)
+        exec_suricata_on_pcap(arguments_parsed)
+        analyze_detection_mode(arguments_parsed)
     else:
         print(ERR_BAD_ARGV)
         sys.exit(1)
-
-
-
-def analyze_all_mode():
-    analyze_visibility_mode()
-    analyze_detection_mode()
 
 # Bootstrap program  
 def main(argv):
     if len(argv) < 2:
         print(ERR_BAD_ARGV) # bad arguments
         sys.exit(1)
+
 
     arguments_parsed = parse_arguments(argv)
 
